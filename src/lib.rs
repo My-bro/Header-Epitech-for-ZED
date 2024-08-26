8bd81c22f69ca377675172421c535c16397bb746
My-bro <axel.londas@epitech.eu>
2024-08-26 23:15:30 +0200

    feat: the needed file

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+use zed_extension_api as zed;
+
+struct Header_Epitech_for_ZED {
+    // ... state
+}
+
+impl zed::Extension for Header_Epitech_for_ZED {
+    // ...
+}
+
+zed::register_extension!(Header_Epitech_for_ZED);
